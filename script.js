b2c08ef111db2afba963a2a028967741d18ad597
Rea-dtech <24582rd@hvhs.school.nz>
2026-02-03 12:22:22 +1300

    3

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+const input1 = document.getElementById("input1");
+let input2 = input1.value;
 let thetext = myTextInput.value;
 alert("Congratulations!\nYou got the script running.")
 function button () {
 alert('hi')
 }
 
-alert (text)
+function alert1 () {
+    alert(input2)
+}
